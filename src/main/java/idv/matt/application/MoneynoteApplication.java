cbb81239e01ae582709fbbb675ab5cf9db0d899e
matt <bronx0807@gmail.com>
2019-02-03 00:14:11 +0800

    aop basic setting

--- src/main/java/idv/matt/application/MoneynoteApplication.java
+++ src/main/java/idv/matt/application/MoneynoteApplication.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "idv.matt.controller" })
+@ComponentScan(basePackages = { "idv.matt.controller, idv.matt.aop" })
 public class MoneynoteApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(MoneynoteApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(MoneynoteApplication.class, args);
+    }
 
 }

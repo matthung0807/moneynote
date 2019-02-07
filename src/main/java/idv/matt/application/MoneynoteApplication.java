94b70f1801dded766b753c6ad1ec5e6487892fd0
matt <bronx0807@gmail.com>
2019-02-08 01:16:06 +0800

    mybatis basic config

--- src/main/java/idv/matt/application/MoneynoteApplication.java
+++ src/main/java/idv/matt/application/MoneynoteApplication.java
@@ -1,11 +1,13 @@
 package idv.matt.application;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "idv.matt.controller, idv.matt.aop" })
+@MapperScan(basePackages = "idv.matt.dao")
 public class MoneynoteApplication {
 
     public static void main(String[] args) {

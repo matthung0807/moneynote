83ae50f221935aad6ce5571897f74fd50d1337b3
matt <bronx0807@gmail.com>
2019-02-08 15:59:13 +0800

    mybatis first test

--- src/main/java/idv/matt/dao/MemberMapper.java
+++ src/main/java/idv/matt/dao/MemberMapper.java
@@ -4,11 +4,14 @@ import idv.matt.entity.Member;
 import java.util.List;
 
 public interface MemberMapper {
+
     int deleteByPrimaryKey(Long memberId);
 
     int insert(Member record);
 
     Member selectByPrimaryKey(Long memberId);
+
+    Member selectByNaturalKey(String memberAccount);
 
     List<Member> selectAll();
 
---
+++ src/test/java/idv/matt/dao/MemberMapperTester.java
@@ -0,0 +1,36 @@
+package idv.matt.dao;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import idv.matt.application.MoneynoteApplication;
+import idv.matt.entity.Member;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = { MoneynoteApplication.class })
+@Transactional
+public class MemberMapperTester {
+
+    @Autowired
+    private MemberMapper mapper;
+
+    @Test
+//    @Rollback
+    public void testInsert() {
+        Member member = new Member();
+        member.setMemberAccount("alice@abc.com");
+        member.setMemberName("Alice");
+        member.setMemberPassword("12345");
+        member.setMemberStatus("1");
+        mapper.insert(member);
+        Member result = mapper.selectByNaturalKey(member.getMemberAccount());
+        Assert.assertEquals(result.getMemberAccount(), member.getMemberAccount());
+    }
+
+}

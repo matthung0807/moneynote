2a6254a6c3e3d8ff4ace9206d480d863a9d1badb
matt <bronx0807@gmail.com>
2019-02-17 12:46:49 +0800

    to JUnit 5

--- src/test/java/idv/matt/controller/HelloControllerTest.java
+++ src/test/java/idv/matt/controller/HelloControllerTest.java
@@ -1,35 +1,30 @@
 package idv.matt.controller;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import idv.matt.application.MoneynoteApplication;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(classes = { MoneynoteApplication.class })
 public class HelloControllerTest {
 
-  @Autowired
-  HelloController helloController;
+    @Autowired
+    HelloController helloController;
 
-  @Test
-  public void testHelloReturnExpectedString() {
-    final String expected = "hello moneynote";
-    final String actual = helloController.hello();
-    assertEquals(expected, actual);
-  }
+    @Test
+    public void testHelloReturnExpectedString() {
+        final String expected = "hello moneynote";
+        final String actual = helloController.hello();
+        Assertions.assertEquals(expected, actual);
+    }
 
-  @Test
-  public void testHelloReturnUnexceptedString() {
-    final String expected = "hello";
-    final String actual = helloController.hello();
-    assertNotEquals(expected, actual);
-  }
+    @Test
+    public void testHelloReturnUnexceptedString() {
+        final String expected = "hello";
+        final String actual = helloController.hello();
+        Assertions.assertNotEquals(expected, actual);
+    }
 
 }
--- src/test/java/idv/matt/dao/MemberMapperTest.java
+++ src/test/java/idv/matt/dao/MemberMapperTest.java
@@ -1,18 +1,15 @@
 package idv.matt.dao;
 
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import idv.matt.application.MoneynoteApplication;
 import idv.matt.entity.Member;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(classes = { MoneynoteApplication.class })
 @Transactional
 public class MemberMapperTest {
@@ -30,7 +27,7 @@ public class MemberMapperTest {
         member.setMemberStatus("1");
         mapper.insert(member);
         Member result = mapper.selectByNaturalKey(member.getMemberAccount());
-        Assert.assertEquals(result.getMemberAccount(), member.getMemberAccount());
+        Assertions.assertEquals(result.getMemberAccount(), member.getMemberAccount());
     }
 
 }
